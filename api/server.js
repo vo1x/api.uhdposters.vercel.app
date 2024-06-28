ef21471872c19a3f2db9e80b8efa759c43ddda52
volx <97158976+vo1x@users.noreply.github.com>
2024-06-28 18:33:13 +0545

    Prepare project for vercel deployment

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const apiRoutes = require("./routes/api");
+const apiRoutes = require("../routes/api");
 
 app.use(
   cors({
