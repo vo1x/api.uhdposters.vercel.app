c6c5124c9e12bf5406081c91a7643234818ce5f6
volx <97158976+vo1x@users.noreply.github.com>
2024-07-03 08:49:05 +0545

    Update server.js

--- api/server.js
+++ api/server.js
@@ -4,19 +4,36 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://uhdposters.vercel.app",
-    optionsSuccessStatus: 200,
-  })
-);
-
 // app.use(
 //   cors({
-//     origin: "http://localhost:5173",
+//     origin: "https://uhdposters.vercel.app",
 //     optionsSuccessStatus: 200,
 //   })
 // );
+
+// app.use(
+//   cors({
+//     origin: "https://c8cb-2400-1a00-bc10-57b5-d5e6-2094-558e-f1d8.ngrok-free.app",
+//     optionsSuccessStatus: 200,
+//   })
+// );
+
+const allowedOrigins = [
+  "https://uhdposters.vercel.app",
+  "https://cloudfiler.vercel.app",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Check if the origin is in the list of allowed origins, or if it is undefined (for non-browser environments)
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 const apiRoutes = require("../routes/api");
 app.use(express.json());
